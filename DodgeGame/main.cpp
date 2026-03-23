01e4e2f666ed8e91f197ee913d52203eb00f3dfc
Marcel Triebe <M4rcel2012@gmail.com>
2026-03-23 13:25:23 +0100

    Restart Nr.2

--- DodgeGame/main.cpp
+++ DodgeGame/main.cpp
@@ -1,23 +1,9 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/OpenGL.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
 
-int main() {
-    sf::RenderWindow window(sf::VideoMode({ 1280, 960 }), "DodgeGame",sf::State::Windowed);
-    window.setFramerateLimit(30);
-
-    window.setActive(true);
-
-    bool gameLoop{ true };
-    while (gameLoop) {
-        // handle events
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-            {
-                // end the program
-                gameLoop = false;
-            }
-        }
-    }
+int main() 
+{
 
 }
