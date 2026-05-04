4642c6f4e67ba4f8761448be2112f4e1df992ac4
Marcel Triebe <M4rcel2012@gmail.com>
2026-05-04 17:29:42 +0200

    Restructured and replanned. Started new implementation.

--- DodgeGame/main.cpp
+++ DodgeGame/main.cpp
@@ -1,9 +1,34 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/OpenGL.hpp>
 #include <SFML/Window.hpp>
-#include <SFML/System.hpp>
 
-int main() 
+const sf::Font font("arial.ttf");
+
+class Button : public sf::Drawable
 {
+public:
+    Button()
+    {}
 
+private:
+    sf::RectangleShape button{ sf::Vector2f(100,50) };
+    sf::Text label{ font, "text", 15 };
+};
+
+
+int main()
+{
+    sf::RenderWindow mainMenu(sf::VideoMode({ 200, 200 }), "SFML works!");
+
+    while (mainMenu.isOpen())
+    {
+        while (const std::optional event = mainMenu.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                mainMenu.close();
+        }
+
+        //mainMenu.clear();
+        mainMenu.display();
+    }
 }
