87c9b982a109aae066215061af75a07879c2274e
Marcel Triebe <M4rcel2012@gmail.com>
2026-05-05 18:04:28 +0200

    Edited the Button Class for the menu

--- DodgeGame/main.cpp
+++ DodgeGame/main.cpp
@@ -2,23 +2,49 @@
 #include <SFML/OpenGL.hpp>
 #include <SFML/Window.hpp>
 
-const sf::Font font("arial.ttf");
 
-class Button : public sf::Drawable
+class Button : public sf::Drawable, sf::Transformable 
 {
 public:
     Button()
-    {}
+    {
+        putButtonTextIntoButton();
+    }
+
+    /*sf::RectangleShape& getButtonShape()
+    {
+        return button;
+    }*/
+
+    
 
 private:
+    
+    void putButtonTextIntoButton()
+    {
+        buttonText.setPosition(button.getOrigin());
+        buttonText.setFillColor(sf::Color::Black);
+    }
+
+    void draw(sf::RenderTarget& target, sf::RenderStates states) const override
+    {
+        target.draw(button, states);
+        target.draw(buttonText, states);
+    }
+
+    std::string buttonID{" "};
+    const sf::Font font{ "arial.ttf" };
     sf::RectangleShape button{ sf::Vector2f(100,50) };
-    sf::Text label{ font, "text", 15 };
+    sf::Text buttonText{ font, "text" };
 };
 
 
 int main()
 {
-    sf::RenderWindow mainMenu(sf::VideoMode({ 200, 200 }), "SFML works!");
+    
+    Button button1{};
+
+    sf::RenderWindow mainMenu(sf::VideoMode({ 1000, 1000 }), "SFML works!");
 
     while (mainMenu.isOpen())
     {
@@ -29,6 +55,7 @@ int main()
         }
 
         //mainMenu.clear();
+        mainMenu.draw(button1);
         mainMenu.display();
     }
 }
